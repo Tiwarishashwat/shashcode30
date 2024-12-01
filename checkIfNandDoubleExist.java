e891ecd1761c2507e39b4105e5bf19723540bb2c
Shashwat Tiwari <47585790+Tiwarishashwat@users.noreply.github.com>
2024-12-01 11:05:28 +0530

    Create checkIfNandDoubleExist.java

---
+++ checkIfNandDoubleExist.java
@@ -0,0 +1,14 @@
+class Solution {
+    // tc-> O(N)
+    // sc-> O(N)
+    public boolean checkIfExist(int[] arr) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int element : arr){
+            if(set.contains(element*2) || ( (element%2==0) && (set.contains(element/2)) )){
+                return true;
+            }
+            set.add(element);
+        }
+        return false;
+    }
+}
